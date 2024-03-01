99441dd18ec3dae75432455f57fb00e373b9ffb6
IlhamArifinnn <arifinn2208@gmail.com>
2024-03-01 22:30:52 +0700

    week2

---
+++ week2/submit.php
@@ -0,0 +1,13 @@
+<?php
+
+$nama = $_POST["nama"];
+$matkul = $_POST["matkul"];
+$nilai_uts = $_POST["nilai_uts"];
+$nilai_uas = $_POST["nilai_uas"];
+$nilai_tugas = $_POST["nilai_tugas"];
+
+echo "nama: $nama <br>";
+echo "matkul: $matkul <br>";
+echo "nilai uts: $nilai_uts <br>";
+echo "nilai uas: $nilai_uas <br>";
+echo "nilai tugas: $nilai_tugas <br>";
