556e82b03576877cfb3086c2492d0d64380b9bb6
IlhamArifinnn <arifinn2208@gmail.com>
2024-03-11 11:08:49 +0700

    edit file week2

--- week2/submit.php
+++ week2/submit.php
@@ -1,10 +1,10 @@
 <?php
 
-$nama = $_POST["nama"];
-$matkul = $_POST["matkul"];
-$nilai_uts = $_POST["nilai_uts"];
-$nilai_uas = $_POST["nilai_uas"];
-$nilai_tugas = $_POST["nilai_tugas"];
+$nama = $_POST["nama"] ?? "";
+$matkul = $_POST["matkul"] ?? "";
+$nilai_uts = $_POST["nilai_uts"] ?? "";
+$nilai_uas = $_POST["nilai_uas"] ?? "";
+$nilai_tugas = $_POST["nilai_tugas"] ?? "";
 
 echo "nama: $nama <br>";
 echo "matkul: $matkul <br>";
